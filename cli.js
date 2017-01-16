7a8e99cd59208ecedda68fc66543caf87f5c16ed
Wang Zuo <wzuoadjusted@gmail.com>
2017-01-16 17:20:23 +0800

    add more channel info

--- cli.js
+++ cli.js
@@ -3,10 +3,12 @@
 const zhibo = require('./lib');
 const url = process.argv[2];
 
-zhibo(url, (err, { live }) => {
-  if (live) {
+zhibo(url, (err, info) => {
+  if (info.live) {
     console.log(`${url} is live`);
   } else {
     console.log(`${url} is not live`);
   }
+
+  console.log(JSON.stringify(info, 2, ' '));
 });
