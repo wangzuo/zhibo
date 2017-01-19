b726ed2f9611cfc7ae449ad61de3d45546748224
Wang Zuo <wzuoadjusted@gmail.com>
2017-01-19 16:28:03 +0800

    output json only

--- cli.js
+++ cli.js
@@ -4,11 +4,5 @@ const zhibo = require('./lib');
 const url = process.argv[2];
 
 zhibo(url, (err, info) => {
-  if (info.live) {
-    console.log(`${url} is live`);
-  } else {
-    console.log(`${url} is not live`);
-  }
-
   console.log(JSON.stringify(info, 2, ' '));
 });
